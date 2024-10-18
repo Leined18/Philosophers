d4791b1fc539e4a80e2ca5a2cb9c622a9ef15116
Leined <erdanielmarciano@hotmail.com>
2024-10-18 12:32:06 +0200

    philo 0.0

--- inc/philo.h
+++ inc/philo.h
@@ -1,7 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 10:44:14 by danpalac          #+#    #+#             */
+/*   Updated: 2024/10/18 11:39:26 by danpalac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
 # include <pthread.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define WHITE "\033[0;37m"
+# define RESET "\033[0m"
+
+typedef struct s_philo
+{
+	int				id;
+	int				left_fork;
+	int				right_fork;
+	int				eat_count;
+	int				is_dead;
+	pthread_t		thread;
+	pthread_mutex_t	forks;
+	pthread_mutex_t	print;
+}					t_philo;
+
+typedef struct s_data
+{
+	int				forks;
+	int				n_philos;
+	int				t_die;
+	int				t_eat;
+	int				t_sleep;
+	int				ntimes_eat;
+	t_philo			*philos;
+
+}					t_data;
 
 #endif
