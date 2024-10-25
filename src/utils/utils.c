dbd599bcc7d7d33c9e589b12538b70431a269a2a
Leined <erdanielmarciano@hotmail.com>
2024-10-25 09:46:03 +0200

    time usleep(50)

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 20:21:44 by danpalac          #+#    #+#             */
-/*   Updated: 2024/10/24 14:53:54 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/10/25 09:42:43 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	smart_sleep(long time_in_ms, t_philo *philo)
 		elapsed_time = get_time() - start_time;
 		if (elapsed_time >= time_in_ms || philo->data->state)
 			return ;
-		usleep(10);
+		usleep(50);
 	}
 }
 
